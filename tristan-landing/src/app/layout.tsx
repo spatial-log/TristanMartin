5cbed99771c40f068255eeebc2594c9f2cbc656e
spatiallog <trimar99@gmail.com>
2026-02-21 15:04:56 -0500

    FEAT: Vercel analytics

--- tristan-landing/src/app/layout.tsx
+++ tristan-landing/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
